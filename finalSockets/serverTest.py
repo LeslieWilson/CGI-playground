1cc46bb56a30e98466a1cafb41a015b4be372cfc
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-05-04 08:36:43 -0400

    client server setup for chatroom

---
+++ finalSockets/serverTest.py
@@ -0,0 +1,39 @@
+# first of all import the socket library
+import socket
+# next create a socket object
+s = socket.socket()
+print "Socket successfully created"
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print "socket binded to %s" %(port)
+
+# put the socket into listening mode
+s.listen(5)
+print "socket is listening"
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+   # Establish connection with client.
+   c, addr = s.accept()
+   print 'Got connection from', addr
+
+   # receive data from the server
+   response =  c.recv(1024)
+
+# name of function
+   chat = newfunction[]
+   # send reversed fortune back to client
+   c.send(chat)
+
+   # Close the connection with the client
+   c.close()
